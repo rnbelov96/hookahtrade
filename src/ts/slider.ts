6d6177e2b260316a07f49c485952150135097b0a
Belov Roman <rnbelov96@gmail.com>
2022-06-29 17:39:45 +0300

    Add new sections

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -31,31 +31,16 @@ const shopsSwiper = new Swiper('.shops__slider', {
   },
 });
 
-// const swiper = new Swiper('.slidersection__slider', {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   loop: true,
-//   centeredSlides: true,
-//   pagination: {
-//     el: '.slidersection__slider-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.slidersection__slider-btn_next',
-//     prevEl: '.slidersection__slider-btn_prev',
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     768: {
-//       slidesPerView: 4,
-//       spaceBetween: 40,
-//     },
-//     1024: {
-//       slidesPerView: 5,
-//       spaceBetween: 50,
-//     },
-//   },
-// });
+const clientsswiper = new Swiper('.clients__slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.clients__slider-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.clients__slider-btn_next',
+    prevEl: '.clients__slider-btn_prev',
+  },
+});
